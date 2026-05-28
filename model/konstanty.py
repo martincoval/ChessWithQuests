aeb05509ef8fff9e34f694a97468910be19d4090
martincoval <martincovalinda7@gmail.com>
2026-05-28 14:07:40 +0200

    Vytvoření konstant barev a vektorů pro usnadnění pohybu vlastních figur.

---
+++ model/konstanty.py
@@ -0,0 +1,18 @@
+BÍLÁ = "bílá"
+ČERNÁ = "černá"
+
+SMERY_VEZ = [
+    (-1; 0),
+    (1; 0),
+    (0; -1),
+    (0; 1)
+]
+
+SMERY_STRELEC = [
+    (-1; -1),
+    (-1; 1),
+    (1; -1),
+    (1; 1)
+]
+
+SMERY_KRAL = SMERY_VEZ + SMERY_STRELEC
