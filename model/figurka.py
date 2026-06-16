63e233ba1366b2f01ae989d3ceae9d0a738eb173
martincoval <martincovalinda7@gmail.com>
2026-06-16 13:57:13 +0200

    Úprava class figurka podle změn v diagramu tříd.

--- model/figurka.py
+++ model/figurka.py
@@ -3,70 +3,52 @@ from abc import ABC, abstractmethod
 Vektor = tuple[int, int]
 
 class Figurka(ABC):
-    def __init__(self, nazev, barva: int, pozice: Vektor):
+    def __init__(self, nazev, barva: int, skok_figury:bool):
         self.nazev:str = nazev
         self.vektory:list[Vektor] = []
         self.vektory_utoku:list[Vektor] = []
         self.barva:int = barva
+        self.skok_figury: bool = skok_figury
 
-        self.skok:bool = False
-
-        self.pozice:Vektor = pozice
-
-    def get_pozice(self) -> Vektor:
-        pass
-
-    def posun_figurky(self, vektor:Vektor):
-        pass
 
 class Jezdec(Figurka):
-    def __init__(self, barva: int, pozice: Vektor):
-        super().__init__("Jezdec", barva, pozice)
+    def __init__(self, barva: int):
+        super().__init__("Jezdec", barva, skok_figury = True)
 
         self.vektory:list[Vektor] = []
         self.vektory_utoku:list[Vektor] = []
-        self.skok:bool = True
 
 class Kral(Figurka):
-    def __init__(self, barva: int, pozice: Vektor):
-        super().__init__("Král", barva, pozice)
+    def __init__(self, barva: int):
+        super().__init__("Král", barva, skok_figury = False)
 
         self.vektory:list[Vektor] = []
         self.vektory_utoku:list[Vektor] = []
-        self.skok:bool = False
 
 class Dama(Figurka):
-    def __init__(self, barva: int, pozice: Vektor):
-        super().__init__("Dáma", barva, pozice)
+    def __init__(self, barva: int):
+        super().__init__("Dáma", barva, skok_figury = False)
 
         self.vektory:list[Vektor] = []
         self.vektory_utoku:list[Vektor] = []
-        self.skok:bool = False
 
 class Vez(Figurka):
-    def __init__(self, barva: int, pozice: Vektor):
-        super().__init__("Věž", barva, pozice)
+    def __init__(self, barva: int):
+        super().__init__("Věž", barva, skok_figury = False)
 
         self.vektory:list[Vektor] = []
         self.vektory_utoku:list[Vektor] = []
-        self.skok:bool = False
 
 class Strelec(Figurka):
-    def __init__(self, barva: int, pozice: Vektor):
-        super().__init__("Střelec", barva, pozice)
+    def __init__(self, barva: int):
+        super().__init__("Střelec", barva, skok_figury = False)
 
         self.vektory:list[Vektor] = []
         self.vektory_utoku:list[Vektor] = []
-        self.skok:bool = False
 
 class Pesec(Figurka):
-    def __init__(self, barva: int, pozice: Vektor):
-        super().__init__("Pěšec", barva, pozice)
+    def __init__(self, barva: int):
+        super().__init__("Pěšec", barva, skok_figury = False)
 
         self.vektory:list[Vektor] = []
         self.vektory_utoku:list[Vektor] = []
-        self.skok:bool = False
-
-
-
-
